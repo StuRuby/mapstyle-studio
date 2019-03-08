7cbe7100bdec527ec68a8dd467ab01f8d9eb0be2
StuRuby <937931249@qq.com>
2019-03-08 14:03:44 +0800

    Add @mapbox/mbx-assembly as UI library

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import '@mapbox/mbx-assembly/dist/assembly.js';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
