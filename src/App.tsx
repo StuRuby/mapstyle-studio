f1fad30b70b56815eca28787eb41704a044e2604
StuRuby <937931249@qq.com>
2019-03-09 01:26:14 +0800

    Update webpack config from antd `Use In Typescript`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
+import DatePicker from 'antd/lib/date-picker';
 import * as React from 'react';
-import EmptyState from './shared_component/empty_state';
 
 const App: React.StatelessComponent<{}> = () => (
     <div>
-        <EmptyState
-            addClassName="mt12 mx12 txt-xs"
-            message="您还没有添加图层，请点击新建图层增加您的第一个图层"
-        />
+        <DatePicker />
+        111
     </div>
 );
 
