432307c7ba4a6b389332cacb86ba41a74f41b7ee
StuRuby <937931249@qq.com>
2019-03-08 16:06:19 +0800

    Add ui component <EmptyComponent&StudioIcon>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
+import EmptyState from './shared_component/empty_state';
 
 const App: React.StatelessComponent<{}> = () => (
     <div>
-        <h1>Hello</h1>
+        <EmptyState
+            addClassName="mt12 mx12 txt-xs"
+            message="您还没有添加图层，请点击新建图层增加您的第一个图层"
+        />
     </div>
 );
 
