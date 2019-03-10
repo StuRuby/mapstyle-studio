e8f9172b129357ae0706be5b8272669800182b53
StuRuby <937931249@qq.com>
2019-03-10 18:05:04 +0800

    Update editor view

--- src/views/Editor.tsx
+++ src/views/Editor.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import LayerEditor from '../containers/layerEditor';
+import LayerList from '../containers/layerList';
+import Map from '../containers/map';
 
 const Editor: React.StatelessComponent<{}> = () => {
-    return <div>Hello Editor</div>;
+    return (
+        <div
+            id="container"
+            className="bg-white select-none absolute top right bottom left"
+        >
+            <Map />
+            <div>
+                <LayerList />
+                <LayerEditor />
+            </div>
+        </div>
+    );
 };
 
 export default Editor;
