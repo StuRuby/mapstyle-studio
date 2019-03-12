6e8487ed64da7e0fb5a8f9768909461399e2433e
StuRuby <937931249@qq.com>
2019-03-12 14:16:24 +0800

    Set svg declareType file to load svg files

--- types/assets.d.ts
+++ types/assets.d.ts
@@ -1,7 +1,9 @@
 declare interface SvgrComponent extends React.StatelessComponent<React.SVGAttributes<SVGElement>> {}
 declare module '*.svg' {
-    const content: SvgrComponent
-    export default content
+    import React = require('react');
+    export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
 }
 
 declare module '*.png' {
