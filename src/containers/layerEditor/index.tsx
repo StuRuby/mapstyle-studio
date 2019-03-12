278a881719e16b969cd045ffac460730b920028c
StuRuby <937931249@qq.com>
2019-03-12 15:13:00 +0800

    Update layerEditor container

--- src/containers/layerEditor/index.tsx
+++ src/containers/layerEditor/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import StyleLayerName from '../../components/layers/StyleLayerName';
 
 interface IProps {
     test?: string;
@@ -12,7 +13,16 @@ export default class LayerEditor extends React.Component<IProps> {
                 className="absolute top left bottom w360 flex-parent flex-parent--column 
                 shadow-darken10 bg-gray-faint border-l border--gray-light"
                 style={{ marginLeft: '210px' }}
-            />
+            >
+                <div
+                    className="flex-child flex-child--no-shrink flex-parent flex-parent--space-between-main 
+                               flex-parent--end-cross bg-gray-faint mx12 border-b border--gray-light"
+                >
+                    <div className="flex-child flex-child--grow py6 my3 wmin0">
+                        <StyleLayerName />
+                    </div>
+                </div>
+            </div>
         );
     }
 }
