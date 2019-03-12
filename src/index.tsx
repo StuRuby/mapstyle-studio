596c51a92fa0aa43fd29ade5eb52d52a4a53f0c7
StuRuby <937931249@qq.com>
2019-03-12 14:20:40 +0800

    Antd says Icon component has an componnet property,it can contains svg component.I tried,but does not work . So fallback to React svg component as the icon

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Router from './router';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root') as HTMLElement );
